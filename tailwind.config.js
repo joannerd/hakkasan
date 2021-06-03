6571d3c43c11669b81e8020dd2010c5574db36c8
Joanna Chen <joannerdchen@gmail.com>
2021-06-02 19:53:07 -0700

    Add custom tailwind breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    screens: {
+      mobile: '600px',
+      tablet: '840px',
+      laptop: '1024px',
+      desktop: '1280px',
+    },
   },
   variants: {
     extend: {},
